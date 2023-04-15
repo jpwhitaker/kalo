f4f7c3ce202fe4f1d60c458fb9bf2dbe28d03e83
JP Whitaker <jpw@hawaii.edu>
2023-04-14 19:13:17 -1000

    hawaiian and english, special characters

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,10 +1,5 @@
-import { PresentationControls, PerspectiveCamera, Text} from '@react-three/drei';
-import { Perf } from 'r3f-perf';
-import Model from './Model.jsx';
-import { Suspense, useEffect } from 'react';
-import Placeholder from './Placeholder.jsx';
-// import Hamburger from './Hamburger.jsx';
-// import Fox from './Fox.jsx';
+import {PresentationControls, OrbitControls, PerspectiveCamera, Text, Billboard, Circle, Sphere} from '@react-three/drei';
+import { useEffect } from 'react';
 import Kalo from './Kalo.jsx';
 import { useThree } from '@react-three/fiber';
 
@@ -12,16 +7,18 @@ export default function Experience() {
   const { camera } = useThree();
 
   useEffect(() => {
-    camera.lookAt(0, 0, 0);
+    camera.lookAt(0, 1, 0);
   }, [camera]);
 
   return (
     <>
-      <PerspectiveCamera makeDefault position={[0, 3.5, 9]} />
+      <PerspectiveCamera makeDefault position={[0, 4, 7.5]} />
 
 
       <ambientLight intensity={0.5} />
-      <PresentationControls makeDefault global snap>
+      {/* <OrbitControls enablePan={true}/>
+       */}
+      <PresentationControls global snap>
       <directionalLight
         castShadow
         position={[1, 2, 3]}
@@ -32,55 +29,58 @@ export default function Experience() {
           receiveShadow
           position-y={-1}
           rotation-x={-Math.PI * 0.5}
-          scale={10}
+          scale={100}
         >
           <planeGeometry />
           <meshStandardMaterial color="greenyellow" />
-          <Kalo scale={0.05} rotation-x={Math.PI * 0.5} position={[0, 0, 0.05]} />
-          
         </mesh>
-        <Text color="black" anchorX="center" anchorY="middle" fontSize={0.5} position={[-5.5,3.5,0]} font={"https://fonts.gstatic.com/s/roboto/v18/KFOmCnqEu92Fr1Mu4mxM.woff"} >
-            lau or lu'au
-        </Text>
-        <Text color="black" anchorX="center" anchorY="middle" fontSize={0.3} position={[-2.5,3.8,0]} font={"https://fonts.gstatic.com/s/roboto/v18/KFOmCnqEu92Fr1Mu4mxM.woff"} >
-            mahae
-        </Text>
-        <Text color="black" anchorX="center" anchorY="middle" fontSize={0.3} position={[-3.5,3,0]} font={"https://fonts.gstatic.com/s/roboto/v18/KFOmCnqEu92Fr1Mu4mxM.woff"} >
-            a'a lau
-        </Text>
-        <Text color="black" anchorX="center" anchorY="middle" fontSize={0.3} position={[-3.5,2.5,0]} font={"https://fonts.gstatic.com/s/roboto/v18/KFOmCnqEu92Fr1Mu4mxM.woff"} >
-            piko
-        </Text>
+        <Kalo scale={0.5}  position={[0, -0.5, 0]} />
+        
+        <CalloutText hawaiianName="mahae" englishTranslation="leaf sinus" position={[-3,4.3,0]} />
+        <Sphere args={[.08,32]} position={[-2,3.6,0.5]} material-color="hotpink"/>
 
-        <Text color="black" anchorX="center" anchorY="middle" fontSize={0.3} position={[-3.5,2,0]} font={"https://fonts.gstatic.com/s/roboto/v18/KFOmCnqEu92Fr1Mu4mxM.woff"} >
-            ka'e lau
-        </Text>
+        <CalloutText hawaiianName="'ao lū'au or mahola" englishTranslation="enexpanded, rolled leaf blade" position={[0,2.5,0]} />
 
-        <Text color="black" anchorX="center" anchorY="middle" fontSize={0.3} position={[0.5,2.5,0]} font={"https://fonts.gstatic.com/s/roboto/v18/KFOmCnqEu92Fr1Mu4mxM.woff"} >
-            'ao lu'au or mahola
-        </Text>
+        <CalloutText hawaiianName="lau or lū'au" englishTranslation="leaf" position={[-6,3.5,0]} />
+        <CalloutText hawaiianName="a'a lau" englishTranslation="midrib and veins" position={[-4.4,3.5,0]} />
+        <CalloutText hawaiianName="piko" englishTranslation="junction of the peticole and blade on upper surface" position={[-4.4,2.8,0]} />
+        <CalloutText hawaiianName="ka'e lau" englishTranslation="edge of leaf" position={[-4.4,1.8,0]} />
 
-        <Text color="black" anchorX="center" anchorY="middle" fontSize={0.3} position={[-1.2,1.5,0]} font={"https://fonts.gstatic.com/s/roboto/v18/KFOmCnqEu92Fr1Mu4mxM.woff"} >
-            ha
-        </Text>
+        <CalloutText hawaiianName="ha" englishTranslation="peticole or leaf stalk" position={[-2,1.8,0]} />
+        <CalloutText hawaiianName="lihi māwae" englishTranslation="sheath edge" position={[-2,1.2,0]} />
+        <CalloutText hawaiianName="māwae" englishTranslation="peticole sheath" position={[-2,0.6,0]} />
 
-
-        <Text color="black" anchorX="center" anchorY="middle" fontSize={0.3} position={[-1.2,1,0]} font={"https://fonts.gstatic.com/s/roboto/v18/KFOmCnqEu92Fr1Mu4mxM.woff"} >
-            lihi mawae
-        </Text>
-
-        <Text color="black" anchorX="center" anchorY="middle" fontSize={0.3} position={[-1.2,0.5,0]} font={"https://fonts.gstatic.com/s/roboto/v18/KFOmCnqEu92Fr1Mu4mxM.woff"} >
-            'oha
-        </Text>
-
-        <Text color="black" anchorX="left" anchorY="middle" fontSize={0.5} position={[1,0,0]} font={"https://fonts.gstatic.com/s/roboto/v18/KFOmCnqEu92Fr1Mu4mxM.woff"} >
-            kalo
-        </Text>
-
-        <Text color="black" anchorX="left" anchorY="middle" fontSize={0.3} position={[0.5,-0.8,0]} font={"https://fonts.gstatic.com/s/roboto/v18/KFOmCnqEu92Fr1Mu4mxM.woff"} >
-            huluhulu
-        </Text>
+        <CalloutText hawaiianName="'ohā" englishTranslation="bud of corm" position={[0.8,0.3,0]} />
+        <CalloutText hawaiianName="huluhulu" englishTranslation="roots" position={[0.8,-0.3,0]} />
       </PresentationControls>
     </>
   );
 }
+
+const CalloutText = ({ hawaiianName, englishTranslation, position }) => {
+  return (
+    <>
+      <Text
+        color="black"
+        anchorX="left"
+        anchorY="top"
+        fontSize={0.2}
+        position={[position[0], position[1], position[2]]}
+        font={"./Roboto-Regular.ttf"}
+      >
+        {hawaiianName}
+      </Text>
+      <Text
+        color="gray"
+        anchorX="left"
+        anchorY="top"
+        fontSize={0.15}
+        font={"./Roboto-Regular.ttf"}
+        position={[position[0], position[1] - 0.25, position[2]]}
+        maxWidth={1.5}
+      >
+        ({englishTranslation})
+      </Text>
+    </>
+  );
+};
